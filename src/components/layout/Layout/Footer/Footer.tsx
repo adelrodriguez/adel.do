914c9a6e2b35fb71eb0f85f2aa77bf1eda63597b
Adel Rodríguez <adelrodriguezv@gmail.com>
2020-10-10 13:00:26 -0400

    Use footer yaml file

--- src/components/layout/Layout/Footer/Footer.tsx
+++ src/components/layout/Layout/Footer/Footer.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 import Typed from 'react-typed';
 
 function Footer() {
+  const { footerContent } = useStaticQuery(
+    graphql`
+      {
+        footerContent: contentYaml(section: { eq: "footer" }) {
+          content
+        }
+      }
+    `
+  );
+
   return (
-    <footer className="font-display text-gray-700 text-sm py-4 border-solid border-t border-gray-200">
+    <footer className="font-display text-gray-700 text-xs md:text-sm py-4 border-solid border-t border-gray-200">
       {`© ${new Date().getFullYear()} — Adel Rodríguez `}
       <Typed
-        strings={[
-          'likes design',
-          'wants your podcast suggestions',
-          'wants to buy a 3D printer',
-          'loves movies',
-          'lives in the Dominican Republic',
-          'loves pepperoni pizza',
-          'is a Star Wars fan',
-          'wants you to have a great day',
-        ]}
+        strings={footerContent.content}
         typeSpeed={40}
         backSpeed={40}
         backDelay={1500}
