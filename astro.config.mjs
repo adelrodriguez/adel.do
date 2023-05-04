7a323d7c5ff720f9bb68d71661b6809eaf7b43d4
Adel Rodríguez <hello@adelrodriguez.com>
2023-05-04 11:44:46 -0400

    migrate profile page to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://adelrodriguez.github.io",
+  integrations: [tailwind()],
+  experimental: {
+    assets: true,
+  },
+})
