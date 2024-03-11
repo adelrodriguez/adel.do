0e2012a294bcb8dcb50c2b6034955fe1a603cd76
Adel Rodríguez <hello@adelrodriguez.com>
2024-03-11 18:44:38 -0400

    move sendawhats.app to adel.do

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss"
+import FormsPlugin from "@tailwindcss/forms"
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [],
+  plugins: [FormsPlugin],
 } satisfies Config
